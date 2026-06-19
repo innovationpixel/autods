f7d333f2c5843bcc8e2b66ec1a80d38d7a1f5e16
Kunzal200 <kunzal.sohail200@gmail.com>
2026-06-19 20:46:24 +0500

    changes

--- src/services/SourcingService.jsx
+++ src/services/SourcingService.jsx
@@ -1,17 +1,33 @@
-import axiosInstance from './AxiosInstance';
-
-export const getSourcingRequests = (params = {}) =>
-    axiosInstance.get('/sourcing/requests', { params });
-
-export const createSourcingRequest = (payload) =>
-    axiosInstance.post('/sourcing/requests', payload);
-
-export const refreshSourcingRequest = (id) =>
-    axiosInstance.post(`/sourcing/requests/${id}/refresh`);
-
-export const linkSourcingQuote = (id, quoteIndex) =>
-    axiosInstance.post(`/sourcing/requests/${id}/link`, { quote_index: quoteIndex });
-
-export const cancelSourcingRequest = (id) =>
-    axiosInstance.post(`/sourcing/requests/${id}/cancel`);
-
+import axiosInstance from './AxiosInstance';
+
+
+
+export const getSourcingRequests = (params = {}) =>
+
+    axiosInstance.get('/sourcing/requests', { params });
+
+
+
+export const createSourcingRequest = (payload) =>
+
+    axiosInstance.post('/sourcing/requests', payload);
+
+
+
+export const refreshSourcingRequest = (id) =>
+
+    axiosInstance.post(`/sourcing/requests/${id}/refresh`);
+
+
+
+export const linkSourcingQuote = (id, quoteIndex) =>
+
+    axiosInstance.post(`/sourcing/requests/${id}/link`, { quote_index: quoteIndex });
+
+
+
+export const cancelSourcingRequest = (id) =>
+
+    axiosInstance.post(`/sourcing/requests/${id}/cancel`);
+
+
